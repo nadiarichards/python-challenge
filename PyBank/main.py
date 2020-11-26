fe91b4f2d378c88df85252fbdb3d792f4de79b93
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2020-11-26 11:27:34 -0500

    solved PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,88 +2,62 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+budget_csv = os.path.join('Resources', 'budget_data.csv')
+file_output = os.path.join('Analysis', 'budget_analysis.txt')
 
 #The total number of months included in the dataset
-#Name first and 2nd column
+# Name first and 2nd column
 # Define the function 
-def print_total (budget_data):
     # For readability, it can help to assign your values to variables with descriptive names
-    date = str(budget_data[0])
-    profit_losses = int(budget_data[1])
+total_months=0
+months_of_change=[]
+net_change_list=[]
+greatest_increase=["",0]
+greatest_decrease=["",99999999999]
+total_net=0
 
-with open(budget_csv, "r") as csvfile:
-
-    # Split the data on commas
-    csvreader = csv.reader(csvfile, delimiter = ",")
-    data = list(csvreader)
-    #header = next(csvreader)
-    number_months= len(data)
-
-
-#number_months = sum(1 for row in csvreader)-1
-
-print (number_months)
-
-#Split data in the first column
-#Len function to count the months?
-
-#The net total amount of "Profit/Losses" over the entire period
-
-#Sum 2nd column
-#$ sign addition
-
-#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
-#Not Sure what that means
-
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
-#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-
-##Text file with the results?
-
- # Total matches can be found by adding wins, losses, and draws together
-    total_matches = wins + losses + draws
-
-    # Win percent can be found by dividing the the total wins by the total matches and multiplying by 100
-    win_percent = (wins / total_matches) * 100
-
-    # Loss percent can be found by dividing the total losses by the total matches and multiplying by 100
-    loss_percent = (losses / total_matches) * 100
-
-    # Draw percent can be found by dividing the total draws by the total matches and multiplying by 100
-    draw_percent = (draws / total_matches) * 100
-
-    # If the loss percentage is over 50, type_of_wrestler is "Jobber". Otherwise it is "Superstar".
-    if loss_percent > 50:
-        type_of_wrestler = "Jobber"
-    else:
-        type_of_wrestler = "Superstar"
-
-    # Print out the wrestler's name and their percentage stats
-    print(f"Stats for {name}")
-    print(f"WIN PERCENT: {win_percent}")
-    print(f"LOSS PERCENT: {loss_percent}")
-    print(f"DRAW PERCENT: {draw_percent}")
-    print(f"{name} is a {type_of_wrestler}")
-
-
-# Read in the CSV file
-with open(wrestling_csv, 'r') as csvfile:
+with open(budget_csv) as csvfile:
 
     # Split the data on commas
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile)
+    header=next(csvreader)
+    first_row=next(csvreader)
+    total_months+=1
+    total_net+=int(first_row[1])
+    previous_net=int(first_row[1])
 
-    header = next(csvreader)
-
-    # Prompt the user for what wrestler they would like to search for
-    name_to_check = input("What wrestler do you want to look for? ")
-
-    # Loop through the data
     for row in csvreader:
 
-        # If the wrestler's name in a row is equal to that which the user input, run the 'print_percentages()' function
-        if name_to_check == row[0]:
-            print_percentages(row)
+        total_months+=1
+        total_net+=int(row[1])
+        net_change=int(row[1])-previous_net
+        previous_net=int(row[1])
+        net_change_list+=[net_change]
+        months_of_change+=[row[0]]
 
+        if net_change > greatest_increase[1]:
+
+            greatest_increase[0]=row[0]
+            greatest_increase[1]=net_change
+
+        if net_change < greatest_decrease[1]:
+
+            greatest_decrease[0]=row[0]
+            greatest_decrease[1]=net_change
+
+net_monthly_average=sum(net_change_list)/len(net_change_list)
+
+output=(
+    f"Financial Analysis\n"
+    f"----------------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average Change: ${net_monthly_average:.2f}\n" 
+    f"Greatest Increase in Profits:{greatest_increase[0]}(${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits:{greatest_decrease[0]}(${greatest_decrease[1]})\n"
+)
+
+print(output)
+
+with open(file_output,"w") as txt_file:
+    txt_file.write(output)
